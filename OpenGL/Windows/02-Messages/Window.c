43b0b53e1a6947bde2f90456e66b35836b0550b5
ezpz4akash <akashmusale100@gmail.com>
2025-01-26 01:38:02 +0530

    Message Handling Windows

--- OpenGL/Windows/02-Messages/Window.c
+++ OpenGL/Windows/02-Messages/Window.c
@@ -50,6 +50,50 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
     TCHAR szBuffer[10];
     switch(uMsg){
+        case WM_CREATE:
+            OutputDebugString(TEXT("This is the first message\n"));
+        break;
+        case WM_SIZE:
+            OutputDebugString(TEXT("Window size is changed\n"));
+        break;
+        case WM_MOVE:
+            OutputDebugString(TEXT("Window is moved\n"));
+        break;
+        case WM_CHAR:
+            switch(wParam){
+                case 'F':
+                    OutputDebugString(TEXT("F Key Is Pressed (WM_CHAR)\n"));
+                break;
+            }
+        break;
+        case WM_KEYDOWN:
+            switch(wParam){
+                case VK_ESCAPE:
+                    OutputDebugString(TEXT("Escape Key Is Pressed (WM_KEYDOWN)\n"));
+                break;
+                case 0x46:
+                    OutputDebugString(TEXT("F VK Key Is Pressed (WM_KEYDOWN)\n"));
+                break;
+                default:
+                break;
+            }
+        break;
+        case WM_KEYUP:
+            switch(wParam){
+                case 0x46:
+                    OutputDebugString(TEXT("F VK Key Is Up (WM_KEYUP)\n"));
+                break;
+                default:
+                break;
+            }
+        break;
+        case WM_LBUTTONDOWN:
+            OutputDebugString(TEXT("Mouse Left Button Is Pressed\n"));
+        break;
+        case WM_CLOSE:
+            OutputDebugString(TEXT("Window is closed (WM_CLOSE)\n"));
+            DestroyWindow(hWnd);
+        break;
         case WM_DESTROY:
             PostQuitMessage(0);
         break;
--- OpenGL/Windows/02-Messages/Window2.c
+++ OpenGL/Windows/02-Messages/Window2.c
@@ -50,6 +50,50 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
     TCHAR szBuffer[10];
     switch(uMsg){
+        case WM_CREATE:
+            MessageBox(hWnd, TEXT("This is the first message"), TEXT("WM_CREATE"), MB_OK);
+        break;
+        case WM_SIZE:
+            MessageBox(hWnd, TEXT("Window size is changed"), TEXT("WM_SIZE"), MB_OK);
+        break;
+        case WM_MOVE:
+            MessageBox(hWnd, TEXT("Window is moved"), TEXT("WM_MOVE"), MB_OK);
+        break;
+        case WM_CHAR:
+            switch(wParam){
+                case 'F':
+                    MessageBox(hWnd, TEXT("F Key Is Pressed"), TEXT("WM_CHAR"), MB_OK);
+                break;
+            }
+        break;
+        case WM_KEYDOWN:
+            switch(wParam){
+                case VK_ESCAPE:
+                    MessageBox(hWnd, TEXT("Escape Key Is Pressed"), TEXT("WM_KEYDOWN"), MB_OK);
+                break;
+                case 0x46:
+                    MessageBox(hWnd, TEXT("F VK Key Is Pressed"), TEXT("WM_KEYDOWN"), MB_OK);
+                break;
+                default:
+                break;
+            }
+        break;
+        case WM_KEYUP:
+            switch(wParam){
+                case 0x46:
+                    MessageBox(hWnd, TEXT("F VK Key Is Up"), TEXT("WM_KEYUP"), MB_OK);
+                break;
+                default:
+                break;
+            }
+        break;
+        case WM_LBUTTONDOWN:
+            MessageBox(hWnd, TEXT("Mouse Left Button Is Pressed"), TEXT("WM_LBUTTONDOWN"), MB_OK);
+        break;
+        case WM_CLOSE:
+            MessageBox(hWnd, TEXT("Window is closed"), TEXT("WM_CLOSE"), MB_OK);
+            DestroyWindow(hWnd);
+        break;
         case WM_DESTROY:
             PostQuitMessage(0);
         break;
