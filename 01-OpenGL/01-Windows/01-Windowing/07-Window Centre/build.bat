ed8f2e0f38ef74e6cb5e6f3503f8f0055bb211d4
ezpz4akash <akashmusale100@gmail.com>
2025-06-18 07:32:36 +0530

    Small Change in Bat file for screenshot

--- 01-OpenGL/01-Windows/01-Windowing/07-Window Centre/build.bat
+++ 01-OpenGL/01-Windows/01-Windowing/07-Window Centre/build.bat
@@ -1,3 +1,3 @@
-cl.exe /c /EHsc window.c
+cl.exe /c /EHsc logfile.c
 rc.exe window.rc
-link.exe window.obj window.res gdi32.lib user32.lib /SUBSYSTEM:WINDOWS
+link.exe logfile.obj window.res gdi32.lib user32.lib /SUBSYSTEM:WINDOWS
